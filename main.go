b3a141e83fa3ae6d32e1222f7505338c10c926d3
soeyusuke <s1240231@gmail.com>
2017-05-07 00:13:38 +0900

    TEST 1

--- main.go
+++ main.go
@@ -1,167 +1,143 @@
 package main
 
 import (
-  "bytes"
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"net/url"
-	"os"
-  "time"
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "net/url"
+    "os"
+    "time"
 )
 
-// FacebookMessenger ...
-type FacebookMessenger struct {
-	Token string
-}
+var accessToken = os.Getenv("ACCESS_TOKEN")
+var verifyToken = os.Getenv("VERIFY_TOKEN")
 
-// CallbackMessage ...
-type CallbackMessage struct {
-	Object string   `json:"object"`
-	Entry  []*Entry `json:"entry"`
+// const ...
+const (
+    EndPoint = "https://graph.facebook.com/v2.6/me/messages"
+)
+
+// ReceivedMessage ...
+type ReceivedMessage struct {
+    Object string  `json:"object"`
+    Entry  []Entry `json:"entry"`
 }
 
 // Entry ...
 type Entry struct {
-	ID        int          `json:"id"`
-	Time      int          `json:"time"`
-	Messaging []*Messaging `json:"messaging"`
+    ID        int64       `json:"id"`
+    Time      int64       `json:"time"`
+    Messaging []Messaging `json:"messaging"`
 }
 
 // Messaging ...
 type Messaging struct {
-	Sender    *ID       `json:"sender"`
-	Recipient *ID       `json:"recipient"`
-	Timestamp int       `json:"timestamp"`
-	Message   *Message  `json:"message,omitempty"`
-	Delivery  *Delivery `json:"delivery,omitempty"`
-	string    `json:""`
+    Sender    Sender    `json:"sender"`
+    Recipient Recipient `json:"recipient"`
+    Timestamp int64     `json:"timestamp"`
+    Message   Message   `json:"message"`
+}
+
+// Sender ...
+type Sender struct {
+    ID int64 `json:"id"`
+}
+
+// Recipient ...
+type Recipient struct {
+    ID int64 `json:"id"`
 }
 
 // Message ...
 type Message struct {
-	Mid  string `json:"mid"`
-	Seq  int    `json:"seq"`
-	Text string `json:"text"`
-}
-
-// Delivery ...
-type Delivery struct {
-	Mids      []string `json:"mids"`
-	Watermark int      `json:"watermark"`
-	Seq       int      `json:"seq"`
-}
-
-// ID ...
-type ID struct {
-	ID int `json:"id"`
-}
-
-// Text ...
-type Text struct {
-	Text string `json:"text"`
+    MID  string `json:"mid"`
+    Seq  int64  `json:"seq"`
+    Text string `json:"text"`
 }
 
 // SendMessage ...
 type SendMessage struct {
-	Recipient *ID   `json:"recipient"`
-	Message   *Text `json:"message"`
+    Recipient Recipient `json:"recipient"`
+    Message   struct {
+        Text string `json:"text"`
+    } `json:"message"`
 }
 
+// func helloHandler(w http.ResponseWriter, r *http.Request) {
+//     fmt.Fprintf(w, "Hello, Facebook Bot")
+// }
 
-var debug bool
-var fb *FacebookMessenger
-
-func callbackHandler(w http.ResponseWriter, r *http.Request) {
-	b, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		fmt.Printf("Something wrong: %s\n", err.Error())
-		return
-	}
-	if debug {
-		log.Println("RecievedMessage Body:", string(b))
-	}
-
-	m, _ := url.ParseQuery(r.URL.RawQuery)
-	fmt.Println(m["hub.verify_token"])
-	if len(m["hub.verify_token"]) > 0 && m["hub.verify_token"][0] == os.Getenv("VERIFY_TOKEN") && len(m["hub.challenge"]) > 0 {
-		fmt.Fprintf(w, m["hub.challenge"][0])
-		return
-	}
-
-	var msg CallbackMessage
-	err = json.Unmarshal(b, &msg)
-	if err != nil {
-		fmt.Printf("Something wrong: %s\n", err.Error())
-		return
-	}
-
-	for _, event := range msg.Entry[0].Messaging {
-		sender := event.Sender.ID
-		if event.Message != nil {
-			fmt.Printf("Recieved Text: %s\n", event.Message.Text)
-			err := fb.SendTextMessage(sender, event.Message.Text)
-			if err != nil {
-				fmt.Printf("Something wrong: %s\n", err.Error())
-			}
-		}
-	}
-
+func webhookHandler(w http.ResponseWriter, r *http.Request) {
+    if r.Method == "GET" {
+        if r.URL.Query().Get("hub.verify_token") == verifyToken {
+            fmt.Fprintf(w, r.URL.Query().Get("hub.challenge"))
+        } else {
+            fmt.Fprintf(w, "Error, wrong validation token")
+        }
+    }
+    if r.Method == "POST" {
+        var receivedMessage ReceivedMessage
+        b, err := ioutil.ReadAll(r.Body)
+        if err != nil {
+            log.Print(err)
+        }
+        if err = json.Unmarshal(b, &receivedMessage); err != nil {
+            log.Print(err)
+        }
+        messagingEvents := receivedMessage.Entry[0].Messaging
+        for _, event := range messagingEvents {
+            senderID := event.Sender.ID
+            if &event.Message != nil && event.Message.Text != "" {
+                sentTextMessage(senderID, event.Message.Text)
+            }
+        }
+        fmt.Fprintf(w, "Success")
+    }
 }
 
-// SendTextMessage ...
-func (fb *FacebookMessenger) SendTextMessage(recipient int, text string) error {
-
-	m := &SendMessage{
-		Recipient: &ID{ID: recipient},
-		Message:   &Text{Text: text},
-	}
-	b, err := json.Marshal(m)
-	if err != nil {
-		return err
-	}
-	req, err := http.NewRequest("POST", "https://graph.facebook.com/v2.6/me/messages?access_token="+fb.Token, bytes.NewBuffer(b))
-	if err != nil {
-		return err
-	}
-
-	req.Header.Add("Content-Type", "application/json; charset=UTF-8")
-
-	client := &http.Client{
-		Timeout: time.Duration(30 * time.Second),
-	}
-	res, err := client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-	var result map[string]interface{}
-	err = json.Unmarshal(body, &result)
-	if err != nil {
-		return err
-	}
-	log.Print("Response: ", result)
-	return nil
-
+func sentTextMessage(senderID int64, text string) {
+    recipient := new(Recipient)
+    recipient.ID = senderID
+    m := new(SendMessage)
+    m.Recipient = *recipient
+    m.Message.Text = text
+    b, err := json.Marshal(m)
+    if err != nil {
+        log.Print(err)
+    }
+    req, err := http.NewRequest("POST", EndPoint, bytes.NewBuffer(b))
+    if err != nil {
+        log.Print(err)
+    }
+    values := url.Values{}
+    values.Add("access_token", accessToken)
+    req.URL.RawQuery = values.Encode()
+    req.Header.Add("Content-Type", "application/json; charset=UTF-8")
+    client := &http.Client{Timeout: time.Duration(30 * time.Second)}
+    res, err := client.Do(req)
+    if err != nil {
+        log.Print(err)
+    }
+    defer res.Body.Close()
+    var result map[string]interface{}
+    body, err := ioutil.ReadAll(res.Body)
+    if err != nil {
+        log.Print(err)
+    }
+    if err := json.Unmarshal(body, &result); err != nil {
+        log.Print(err)
+    }
+    log.Print(result)
 }
 
 
 func main() {
-	debug = true
-	fb = &FacebookMessenger{
-		Token: os.Getenv("ACESS_TOKEN"),
-	}
-
-	http.HandleFunc("/fbbot/callback", callbackHandler)
-
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
-	http.ListenAndServe(addr, nil)
+    //http.HandleFunc("/", helloHandler)
+    http.HandleFunc("/webhook", webhookHandler)
+    port := os.Getenv("PORT")
+    addr := fmt.Sprintf(":%s", port)
+    http.ListenAndServe(addr, nil)
 }
