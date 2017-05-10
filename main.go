c42c98660c23ffd497d62b92d578b0158baa539f
soeyusuke <s1240231@gmail.com>
2017-05-10 21:38:55 +0900

    change part13

--- main.go
+++ main.go
@@ -62,58 +62,59 @@ type Message struct {
 
 // SendMessage ...
 type SendMessage struct {
-	Sender    Sender    `json:"sender"`
 	Recipient Recipient `json:"recipient"`
 	Message struct {
 		Text string `json:"text"`
 	} `json:"message"`
 }
 
-func main(){
-	// handleReceiveMessage = callback
-	http.HandleFunc("/", helloHandler)
+func main() {
+	http.HandleFunc("/", TopPageHandler)
 	http.HandleFunc("/webhook", webhookHandler)
-	port := os.Getenv("PORT")
-	addr := fmt.Sprintf(":%s", port)
-	http.ListenAndServe(addr, nil)
+	address := fmt.Sprintf(":%s", port)
+	http.ListenAndServe(address, nil)
 }
 
-// var handleReceiveMessage func(Messaging)
-
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello, Facebook Bot")
+func TopPageHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "This is go-bot application's top page.")
 }
 
 func webhookHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
-		if r.URL.Query().Get("hub.verify_token") == verifyToken {
-			fmt.Fprintf(w, r.URL.Query().Get("hub.challenge"))
-		} else {
-			fmt.Fprintf(w, "Error, wrong validation token")
-		}
+		verifyTokenAction(w, r)
 	}
-
 	if r.Method == "POST" {
-		var receivedMessage ReceivedMessage
-		b, err := ioutil.ReadAll(r.Body)
-
-		if err != nil {
-			log.Print(err)
-		}
-		if err = json.Unmarshal(b, &receivedMessage); err != nil {
-			log.Print(err)
-		}
-
-		messagingEvents := receivedMessage.Entry[0].Messaging
-		for _, event := range messagingEvents {
-			senderID := event.Sender.ID
-
-			if &event.Message != nil && event.Message.Text != "" {
-				sendTextMessage(senderID, event.Message.Text)
-			}
-		}
-		fmt.Fprintf(w, "Success")
+		webhookPostAction(w, r)
 	}
+}
+
+func verifyTokenAction(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Query().Get("hub.verify_token") == verifyToken {
+		log.Print("verify token success.")
+		fmt.Fprintf(w, r.URL.Query().Get("hub.challenge"))
+	} else {
+		log.Print("Error: verify token failed.")
+		fmt.Fprintf(w, "Error, wrong validation token")
+	}
+}
+
+func webhookPostAction(w http.ResponseWriter, r *http.Request) {
+	var receivedMessage ReceivedMessage
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Print(err)
+	}
+	if err = json.Unmarshal(body, &receivedMessage); err != nil {
+		log.Print(err)
+	}
+	messagingEvents := receivedMessage.Entry[0].Messaging
+	for _, event := range messagingEvents {
+		senderID := event.Sender.ID
+		if &event.Message != nil && event.Message.Text != "" {
+			sendTextMessage(senderID, event.Message.Text)
+		}
+	}
+	fmt.Fprintf(w, "Success")
 }
 
 func sendTextMessage(senderID int64, text string) {
